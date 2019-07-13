b508557c86d0c5a4acdb8d1651605d810c772007
MILL5 <admin@mill5.com>
2019-07-13 03:10:35 -0400

    Initial checkin

---
+++ M5.Common/Cleanup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using static M5.Common.Preconditions;
+// ReSharper disable UnusedMember.Global
+
+namespace M5.Common
+{
+    public static class Cleanup
+    {
+        public interface ISafeContinue
+        {
+            ISafeContinue Continue(Action a);
+            ISafeContinue OnSuccess(Action a);
+            ISafeContinue OnFailure(Action a);
+        }
+
+        private class SafeContinue : ISafeContinue
+        {
+            private readonly bool _success;
+
+            public SafeContinue(bool success)
+            {
+                _success = success;
+            }
+
+            public ISafeContinue Continue(Action a)
+            {
+                CheckIsNotNull(nameof(a), a);
+
+                SafeMethod(a);
+
+                return new SafeContinue(true);
+            }
+
+            public ISafeContinue OnSuccess(Action a)
+            {
+                CheckIsNotNull(nameof(a), a);
+
+                if (_success)
+                {
+                    return SafeMethod(a);
+                }
+
+                return new SafeContinue(false);
+            }
+
+            public ISafeContinue OnFailure(Action a)
+            {
+                CheckIsNotNull(nameof(a), a);
+
+                if (!_success)
+                {
+                    return SafeMethod(a);
+                }
+
+                return new SafeContinue(true);
+            }
+        }
+
+        public static ISafeContinue SafeMethod(Action a)
+        {
+            bool success = false;
+
+            CheckIsNotNull(nameof(a), a);
+
+            try
+            {
+                a();
+                success = true;
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return new SafeContinue(success);
+        }
+    }
+}
---
+++ M5.Common/ShortGuid.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using static M5.Common.Preconditions;
+// ReSharper disable UnusedMember.Global
+
+namespace M5.Common
+{
+    public struct ShortGuid
+    {
+        public static readonly ShortGuid Empty = new ShortGuid(Guid.Empty);
+
+        private Guid _guid;
+        private string _value;
+
+        public ShortGuid(string value)
+        {
+            CheckIsNotNullOrWhitespace(nameof(value), value);
+
+            _value = value;
+            _guid = Decode(value);
+        }
+
+        public ShortGuid(Guid guid)
+        {
+            _value = Encode(guid);
+            _guid = guid;
+        }
+
+        public Guid Guid
+        {
+            get => _guid;
+            set
+            {
+                if (value != _guid)
+                {
+                    _guid = value;
+                    _value = Encode(value);
+                }
+            }
+        }
+
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                if (value != _value)
+                {
+                    _value = value;
+                    _guid = Decode(value);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return _value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ShortGuid guidAsShortGuid)
+                return _guid.Equals(guidAsShortGuid._guid);
+
+            if (obj is Guid guid)
+                return _guid.Equals(guid);
+
+            if (obj is string guidAsString)
+                return _guid.Equals(new Guid(guidAsString));
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            // ReSharper disable once NonReadonlyMemberInGetHashCode
+            return _guid.GetHashCode();
+        }
+
+        public static ShortGuid NewGuid()
+        {
+            return new ShortGuid(Guid.NewGuid());
+        }
+
+        public static string Encode(string value)
+        {
+            Guid guid = new Guid(value);
+            return Encode(guid);
+        }
+
+        public static string Encode(Guid guid)
+        {
+            string encoded = Convert.ToBase64String(guid.ToByteArray());
+            encoded = encoded
+              .Replace("/", "_")
+              .Replace("+", "-");
+            return encoded.Substring(0, 22);
+        }
+
+        public static Guid Decode(string value)
+        {
+            value = value
+              .Replace("_", "/")
+              .Replace("-", "+");
+            byte[] buffer = Convert.FromBase64String(value + "==");
+            return new Guid(buffer);
+        }
+
+        public static bool operator ==(ShortGuid x, ShortGuid y)
+        {
+            return x._guid == y._guid;
+        }
+
+        public static bool operator !=(ShortGuid x, ShortGuid y)
+        {
+            return !(x == y);
+        }
+
+        public static implicit operator string(ShortGuid shortGuid)
+        {
+            return shortGuid._value;
+        }
+
+        public static implicit operator Guid(ShortGuid shortGuid)
+        {
+            return shortGuid._guid;
+        }
+
+        public static implicit operator ShortGuid(string shortGuid)
+        {
+            return new ShortGuid(shortGuid);
+        }
+
+        public static implicit operator ShortGuid(Guid guid)
+        {
+            return new ShortGuid(guid);
+        }
+    }
+}
