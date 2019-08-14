062dac078f2dc86d26cfb57c77dfbd31a903ea2b
Rich Crane <rich@mill5.com>
2019-08-14 09:59:20 -0400

    ConsoleColor

---
+++ Pineapple/Diagnostics/ConsoleColorScope.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static Pineapple.Common.Preconditions;
+
+namespace Pineapple.Diagnostics
+{
+    public class ConsoleColorScope : IDisposable
+    {
+        private ConsoleColor _savedColor;
+
+        public ConsoleColorScope(ConsoleColor consoleColor)
+        {
+            CheckIsNotNull(nameof(consoleColor), consoleColor);
+
+            _savedColor = Console.ForegroundColor;
+            Console.ForegroundColor = consoleColor;
+        }
+
+        public void Dispose()
+        {
+            Console.ForegroundColor = _savedColor;
+        }
+    }
+}
