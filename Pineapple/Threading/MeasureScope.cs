41413614e867d8b2bbaeecb8807d5ebb2128d2a0
Richard Crane <rich@mill5.com>
2021-03-17 07:45:46 -0400

    Updated Resource Governor

---
+++ Pineapple/Threading/MeasureScope.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pineapple.Threading
+{
+    public class MeasureScope : IDisposable, IAsyncDisposable
+    {
+        private readonly Stopwatch _sw;
+        private readonly string _token;
+        private long _numOfCalls;
+
+        public MeasureScope([CallerMemberName] string token = null)
+        {
+            _token = token ?? nameof(MeasureScope);
+            _sw = new Stopwatch();
+            _sw.Start();
+        }
+        public void Count(Action countThis)
+        {
+            try
+            {
+                countThis();
+                Interlocked.Increment(ref _numOfCalls);
+            }
+            catch
+            {
+                Debug.WriteLine($@"[{_token}] Error detected.");
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            _sw.Stop();
+
+            var c = Interlocked.Read(ref _numOfCalls);
+            var cpm = c * 1.0 / _sw.ElapsedMilliseconds;
+
+            Debug.WriteLine($@"[{_token}] {c} calls, CPM={cpm}");
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            _sw.Stop();
+            await Task.CompletedTask;
+        }
+    }
+}
